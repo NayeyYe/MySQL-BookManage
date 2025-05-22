fcc9cc291ac906571f2252035167bb0e46f60af1
NayeyYe <Nayey.Ye@outlook.com>
2025-05-22 23:24:46 +0800

    创建账号密码表

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -39,6 +39,14 @@ create table if not exists borrower(
 );
 
 
+# 账号密码表
+drop table if exists user_info;
+create table if not exists user_info(
+    id int comment '账号',
+    password_hash VARCHAR(255) NOT NULL COMMENT 'bcrypt密码哈希'
+);
+
+
 # 出版社表
 drop table if exists publisher;
 create table if not exists publisher(
